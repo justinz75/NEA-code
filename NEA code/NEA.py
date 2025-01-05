8458c112fd1a3e3c58e934f43cfa4735213e009c
justinz75 <zihang121@gmail.com>
2025-01-05 23:22:38 +0000

    nea

--- NEA code/NEA.py
+++ NEA code/NEA.py
@@ -1,11 +1,16 @@
 #import libraries
 import pygame
 import sys
+import random
 
 #game window constants
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 FPS = 60
+
+#Track segment probabilities
+STRAIGHT_PROBABILITY = 0.9
+TURN_PROBABILITY = 0.1
 
 #Game class
 class Game:
@@ -22,12 +27,13 @@ class Game:
         self.pause = Pause(self.screen, self.gameStateManager)
         self.high_scores = High_Scores(self.screen, self.gameStateManager)
         self.results = Results(self.screen, self.gameStateManager)
+        self.play = Playing(self.screen, self.gameStateManager)
         #initialise buttons for main menu
         self.play_button = pygame.Rect(855, 350, 170, 100)
         self.game_controls_button = pygame.Rect(633, 520, 615, 100)
         self.quit_button = pygame.Rect(856, 700, 157, 100)
         #dictionary of game states
-        self.states = {'main menu': self.main_menu, 'menu': self.menu, 'game controls': self.game_controls, 'pause': self.pause, 'high scores': self.high_scores, 'results': self.results}
+        self.states = {'main menu': self.main_menu, 'menu': self.menu, 'game controls': self.game_controls, 'pause': self.pause, 'high scores': self.high_scores, 'results': self.results, 'play': self.play}
     
     def run(self):
         run = True
@@ -57,10 +63,12 @@ class Game:
             self.key = pygame.key.get_pressed()
             if self.key[pygame.K_m] and (self.gameStateManager.get_state() == 'menu' or self.gameStateManager.get_state() == 'game controls'):
                 self.gameStateManager.set_state('main menu')
-                
+            if self.key[pygame.K_c] and self.gameStateManager.get_state() == 'menu':
+                self.gameStateManager.set_state('play')
+                self.play.track.run()
             #run game states
             self.states[self.gameStateManager.get_state()].run()
-            #pygame.draw.rect(self.screen, (0, 0, 0), self.quit_button)
+            #pygame.draw.rect(self.display, (0, 0, 0), self.results_button)
             #updates the display
             pygame.display.update()
             #ensures game runs at the correct fps
@@ -155,6 +163,46 @@ class Results:
     #displays the results
     def run(self):
         self.display.blit(self.background_image, (0, 0))
+
+#Playing class
+class Playing:
+    #initialises display and gameStateManager
+    def __init__(self, display, gameStateManager):
+        self.display = display
+        self.gameStateManager = gameStateManager
+        self.track = Track(self.display, self.gameStateManager)
+    #displays the results
+    def run(self):
+        self.track.run()
+
+#Track class
+class Track:
+    #initialises display and gameStateManager
+    def __init__(self, display, gameStateManager):
+        self.display = display
+        self.gameStateManager = gameStateManager
+        self.track_segments = []
+        self.generate_track()
+    
+    def generate_track(self):
+        # Generate track segments based on probabilities
+        for _ in range(30):  # Adjust the range for the desired track length
+            if random.random() < STRAIGHT_PROBABILITY:
+                self.track_segments.append('straight')
+            else:
+                self.track_segments.append('turn')
+    
+    #displays the results
+    def run(self):
+        y_offset = 0
+        for segment in self.track_segments:
+            if segment == 'straight':
+                background_image = pygame.image.load('C:/NEA/NEA sprites/Straight.png').convert_alpha()
+            else:
+                background_image = pygame.image.load('C:/NEA/NEA sprites/Turn.png').convert_alpha()
+            background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT // len(self.track_segments)))
+            self.display.blit(background_image, (0, y_offset))
+            y_offset += SCREEN_HEIGHT // len(self.track_segments)
         
 if __name__ == '__main__':
     #creates object 'game' of class Game
